2bf92330fa935f6bf2f6c6695ceb399dd07082d5
Rafhael-Josino <rafhael_josino1@hotmail.com>
2023-04-14 10:59:28 -0300

    App.tsx updated without App Loading component

--- App.tsx
+++ App.tsx
@@ -5,15 +5,18 @@ import { StatusBar } from 'react-native';
 import { useFonts, Rajdhani_400Regular } from '@expo-google-fonts/rajdhani';
 import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
+import { View, Text } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Rajdhani_400Regular,
   });
 
-  // if (!fontsLoaded) {
-  //   return <AppLoading />
-  // }
+  if (!fontsLoaded) {
+    return <View>
+      <Text>Loading</Text>
+    </View>
+  }
 
   return <ThemeProvider theme={theme}>
     <Dashboard />
