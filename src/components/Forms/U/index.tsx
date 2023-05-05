1d96bd35ce1862a7caae52e87486d9df57bea451
rafhael josino <rafhael_josino1@hotmail.com>
2023-05-05 00:32:07 -0300

    Gesture handler added to each space of U selection

--- src/components/Forms/U/index.tsx
+++ src/components/Forms/U/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { TouchableOpacityProps } from "react-native/types";
+import { RectButton } from "react-native-gesture-handler";
 
 import { Container, U_text } from "./styles";
+import theme from "../../../global/styles/theme";
 
 interface Props extends TouchableOpacityProps {
     asset: string; // for now receives only the name
@@ -13,9 +15,13 @@ export function U(props: Props) {
     const { asset, index, selected, ...rest } = props;
 
     
-    return <Container {...rest}>
+    return <RectButton
+      rippleColor={theme.colors.headerBackground}
+    >
+      <Container {...rest}>
         <U_text asset={asset} selected={selected}>{index+1}</U_text>
         <U_text asset={asset} selected={selected}>{asset}</U_text>
         <U_text asset={asset} selected={selected}>{index+1}</U_text>
-    </Container>
+      </Container>
+    </RectButton>
 }
